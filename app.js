0eec0586383d19fe4d15943104f2e9fcbcf36549
saksham <sakshamgupta034@gmial.com>
2020-09-14 22:51:47 -0700

    Deploying

--- app.js
+++ app.js
@@ -19,4 +19,12 @@ app.all('*', (req, res, next) => {
     });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 module.exports = app
