8ab475f0a530cbc1a03ee168f5a7fd5a13d6b0f3
saksham <sakshamgupta034@gmial.com>
2020-09-15 23:42:24 -0700

    Applied css to signup page

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react'
+import {Link} from 'react-router-dom'
 import { connect } from 'react-redux';
 import { register } from './../../actions/auth'
 import PropTypes from 'prop-types';
@@ -26,45 +27,62 @@ const Signup = ({ register }) => {
         }
     };
 
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <div className="form-group">
-                    <input
+    return ( 
+        <div  className='forlogin'>
+      <div className='login_box'> 
+        <div className='login_box_left_signup'>
+          {/* <img src='https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt='' /> */}
+        </div>
+        <div className='login_box_right'>
+        <div className='login_box_right_padd'>
+            <h1 className="text-primary">Join Now !</h1>
+            <p className='text-secondary'>Fill Your Data to enter . Thank You.</p>
+            <p className="lead">
+              {/* <i className="fas fa-user" /> Sign Into Your Account */}
+            </p>
+            <form className="form" onSubmit={onSubmit}>
+              <div className="form-group">
+                <p>User</p>
+                <input
                         type="text"
                         name="name"
                         value={name}
                         onChange={onChange} />
-
-                </div>
-                <div className="form-group">
-                    <input
+              </div>
+              <div className="form-group">
+              <p>Email-id</p>
+              <input
                         type="email"
                         name="email"
                         value={email}
                         onChange={onChange}
                     />
-
-                </div>
-                <div className="form-group">
-                    <input
+              </div>
+              <div className="form-group">
+              <p>Password</p>
+              <input
                         type="password"
                         name="password"
                         value={password}
                         onChange={onChange} />
-
-                </div>
-                <div className="form-group">
-                    <input
+              </div>
+              <div className="form-group">
+              <p>Confirm Password</p>
+              <input
                         type="password2"
                         name="password2"
                         value={password2}
                         onChange={onChange} />
-                </div>
-                <input type="submit" value="Register" />
+              </div>
+              <input type="submit" className="btn btn-primary" value="Login" />
             </form>
-
+            <p className="my-1">
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
         </div>
+        </div>
+      </div>
+      </div>
     )
 }
 
